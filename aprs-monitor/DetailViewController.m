c028d4f629fa41f65c5d2559cc7b3e6ce5fb0ce6
Alex Lelievre <alex@folabs.com>
2020-08-14 23:43:08 -0700

    Pretty good stubs.  Probably should refactor date routines.

--- aprs-monitor/DetailViewController.m
+++ aprs-monitor/DetailViewController.m
@@ -10,6 +10,7 @@
 #import "Packet.h"
 
 
+
 @interface DetailGenericCell : UITableViewCell
 @property (weak, nonatomic) IBOutlet UILabel* name;
 @property (weak, nonatomic) IBOutlet UILabel* data;
@@ -33,6 +34,8 @@
 
 
 @interface DetailViewController ()
+@property (nonatomic, strong) NSDateFormatter* dateFormatter;
+@property (nonatomic, strong) NSDateFormatter* timeFormatter;
 @end
 
 @implementation DetailViewController
@@ -40,12 +43,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    _dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    _dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+    _dateFormatter.locale    = [NSLocale currentLocale];
+    _dateFormatter.doesRelativeDateFormatting = YES;
+    
+    _timeFormatter = [[NSDateFormatter alloc] init];
+    _timeFormatter.timeStyle = NSDateFormatterMediumStyle;
+    _timeFormatter.dateStyle = NSDateFormatterNoStyle;
+    _timeFormatter.locale    = [NSLocale currentLocale];
+
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
+
+
+- (NSString*)getDateString:(NSDate*)date
+{
+    NSTimeZone* timeZone = [NSTimeZone localTimeZone];
+    NSString* gmt = [timeZone abbreviationForDate:date];
+
+    return [NSString stringWithFormat:@"%@ %@ %@", [_dateFormatter stringFromDate:date], [_timeFormatter stringFromDate:date], gmt];
+}
+
 
 #pragma mark - Table view data source
 
@@ -54,48 +78,87 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return 6;
 }
 
 
 // detail.wind.cell
 - (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath {
-//    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
-    WindCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.wind.cell" forIndexPath:indexPath];
-
-    // Configure the cell...
-    cell.line1.text = @"wind speed and direction";
-    cell.line2.text = @"gust: blah mph";
     
-
-    
-
-    // !!@ test code
-    Packet* pkt = [[Packet alloc] init];
-    pkt.flags |= (kPacketFlag_CoordinatesValid | kPacketFlag_Weather);
-    pkt.coordinate = CLLocationCoordinate2DMake( -34.5, 118.20 );
-    pkt.call = @"K6TEST";
-    pkt.weather = @"fake weather";
-    pkt.symbol = @"/_";
-    
-    pkt.wx = malloc( sizeof( wx_data ) );
-    if( pkt.wx )
+    if( indexPath.row == 0 )
     {
-        pkt.wx->wxflags |= (kWxDataFlag_gust | kWxDataFlag_windDir | kWxDataFlag_wind | kWxDataFlag_temp | kWxDataFlag_humidity | kWxDataFlag_pressure);
-        pkt.wx->windGustMph = 10;
-        pkt.wx->windSpeedMph = 2;
-        pkt.wx->windDirection = 195;
+        // !!@ test code
+        WindCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.wind.cell" forIndexPath:indexPath];
 
-        pkt.wx->tempF    = 100;
-        pkt.wx->humidity = 55;
-        pkt.wx->pressure = 1013;
-     
-        pkt.weather = [Packet makeWeatherString:pkt.wx];
+        // Configure the cell...
+        cell.line1.text = @"wind speed and direction";
+        cell.line2.text = @"gust: blah mph";
+
+        Packet* pkt = [[Packet alloc] init];
+        pkt.flags |= (kPacketFlag_CoordinatesValid | kPacketFlag_Weather);
+        pkt.coordinate = CLLocationCoordinate2DMake( -34.5, 118.20 );
+        pkt.call = @"K6TEST";
+        pkt.weather = @"fake weather";
+        pkt.symbol = @"/_";
+        
+        pkt.wx = malloc( sizeof( wx_data ) );
+        if( pkt.wx )
+        {
+            pkt.wx->wxflags |= (kWxDataFlag_gust | kWxDataFlag_windDir | kWxDataFlag_wind | kWxDataFlag_temp | kWxDataFlag_humidity | kWxDataFlag_pressure);
+            pkt.wx->windGustMph = 10;
+            pkt.wx->windSpeedMph = 2;
+            pkt.wx->windDirection = 195;
+
+            pkt.wx->tempF    = 100;
+            pkt.wx->humidity = 55;
+            pkt.wx->pressure = 1013;
+         
+            pkt.weather = [Packet makeWeatherString:pkt.wx];
+        }
+        
+        cell.windIcon.image = [pkt getWindIndicatorIcon:CGRectMake( 0, 0,  40, 40 )];
+        return cell;
+    }
+    else if( indexPath.row == 1 )
+    {
+        DetailGenericCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
+        cell.name.text = @"Received";
+        cell.data.text = [self getDateString:[NSDate now]];
+        return cell;
+    }
+    else if( indexPath.row == 2 )
+    {
+        DetailGenericCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
+        cell.name.text = @"Latitude";
+        cell.data.text = @"34.45345N";
+        return cell;
+    }
+    else if( indexPath.row == 3 )
+    {
+        DetailGenericCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
+        cell.name.text = @"Longitude";
+        cell.data.text = @"118.45345W";
+        return cell;
+    }
+    else if( indexPath.row == 4 )
+    {
+        DetailGenericCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
+        cell.name.text = @"Comment";
+        cell.data.text = @"blah blah";
+        return cell;
+    }
+    else if( indexPath.row == 5 )
+    {
+        DetailGenericCell* cell = [tableView dequeueReusableCellWithIdentifier:@"detail.generic.field" forIndexPath:indexPath];
+        cell.name.text = @"Path";
+        cell.data.text = @"WEER>WEREWR-DSFDSF->BEER";
+        return cell;
+    }
+    else
+    {
     }
     
-    cell.windIcon.image = [pkt getWindIndicatorIcon:CGRectMake( 0, 0,  40, 40 )];
-    
-    return cell;
+    return nil;
 }
 
 
